78610b9f282f74bf96f159f4be505d5df95116b6
Jul Lang <jullanggit@proton.me>
2025-03-03 00:45:11 +0100

    some little code improvements

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,11 @@ const SECONDS_PER_HOUR: u64 = 60 * 60;
 const SECONDS_PER_DAY: u64 = SECONDS_PER_HOUR * 24;
 
 fn main() {
-    let home = env::var("HOME").unwrap();
-    let string = fs::read_to_string(format!("{home}/.config/sleep-timer")).unwrap();
-    let mut lines = string.lines();
+    // Get the config file
+    let config_string =
+        fs::read_to_string(format!("{}/.config/sleep-timer", env::var("HOME").unwrap())).unwrap();
+
+    let mut lines = config_string.lines();
 
     let hour_offset: i64 = lines.next().unwrap().parse().unwrap();
 
@@ -24,20 +26,21 @@ fn main() {
 
     assert!(times.len() == 7);
 
-    let duration = SystemTime::now()
+    let time = SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .expect("Time went backwards");
 
-    let days = duration.as_secs() / SECONDS_PER_DAY;
+    let days = time.as_secs() / SECONDS_PER_DAY;
     let day_of_week = (days + 4) % 7; // 0 = Monday
 
-    let seconds_into_day = duration.as_secs() % SECONDS_PER_DAY;
+    let seconds_into_day = time.as_secs() % SECONDS_PER_DAY;
 
     let hour = ((seconds_into_day / SECONDS_PER_HOUR) as i64 + hour_offset) as u64;
     let minute = (seconds_into_day % SECONDS_PER_HOUR) / 60;
 
     let (target_hour, target_minute) = times[day_of_week as usize];
 
+    // Calculate duration until target hour
     let duration = {
         let hours = Duration::from_hours(if target_hour < hour {
             24 - (hour - target_hour)
@@ -50,7 +53,9 @@ fn main() {
             hours + Duration::from_mins(target_minute - minute)
         }
     }
+    // Apply timer (time-zone) offset
     .checked_sub(Duration::from_mins(timer_offset.0 * 60 + timer_offset.1))
+    // If were past the sleep time already, wait for 15 minutes
     .unwrap_or(Duration::from_mins(15));
 
     println!("Waiting for: {} min", duration.as_secs() / 60);
